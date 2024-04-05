0ae9176ffe2ae5ba540c84f6fe2700dc678ec9b7
eenrics <ebenezeresh525@gmail.com>
2024-04-05 10:03:51 +0300

    feat: test cases added for films component and service

--- src/app/services/films.service.spec.ts
+++ src/app/services/films.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FilmsService } from './films.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('FilmsService', () => {
   let service: FilmsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler]
+    });
     service = TestBed.inject(FilmsService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should have getFilm method', () => {
+    expect(service.getFilm).toBeDefined();
   });
 });
